4b5a26c008239c6ac40df130fa8af445febf3a81
Martin Holland <martin.holland@free.fi>
2023-09-08 11:20:15 +0300

    add redux functions for addToCart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,19 @@
+import { Container } from "react-bootstrap";
+import { useAppSelector } from "../app/hooks";
+import Product from "./Product";
+
 const Cart = () => {
+    const cartItems = useAppSelector(state => state.cart.cart);
+    console.log("CartItems; ", cartItems)
 
     return (
-        <div>Cart will be here</div>
+        <Container>
+            <h1>Cart will be here</h1>
+            {cartItems.length === 0 && <p>Your cart is empty</p>}
+            {cartItems.map((item) => (
+                 <Product {...item} />
+            ))}
+        </Container>
     )
 }
 
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,12 +1,36 @@
-const Product = (props) => {
-    const { image, title } = props;
+import { Button } from "react-bootstrap";
+import { useAppDispatch } from "../app/hooks";
+import { addToCart } from "../features/cartSlice";
 
+const Product = (props) => {
+    const { image, title, price, rating, description } = props;
+    const product = props
+    const dispatch = useAppDispatch();
+
+const handleAddProduct = () => {
+    console.log("Product: ", product)
+    dispatch(addToCart(product))
+}
 
 return (
     <section className="Detail">
+
         <article className="Detail_thumbnail">
             <img src={image} alt={title} />
         </article>
+
+        <article className="Detail_info">
+            <div className="Detail_info-header">
+            <h2>{title}</h2>
+            </div>
+            <div classNAme="Detail_info">
+                <span className="Detail_info-price">{price}</span>
+                <span className="Detail_info-rating">Rating: {rating.rate}</span>
+            </div>
+            <p className="Detail_info-description">{description}</p>
+            <Button variant="primary" onClick={handleAddProduct}>Add to Cart</Button>
+        </article>
+
     </section>
 )
 }
